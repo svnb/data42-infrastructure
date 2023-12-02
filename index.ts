4427235c47f9bde44e7d3c54152dfb359c040628
svnb <sven@bytecause.ch>
2023-12-02 22:56:07 +0100

    prettier

--- index.ts
+++ index.ts
@@ -2,14 +2,14 @@ import * as pulumi from "@pulumi/pulumi";
 import { Datahub } from "./src/datahub";
 
 interface InfrastructureArgs {
-    datahub: {
-        namespace: string
-    }
+  datahub: {
+    namespace: string;
+  };
 }
 
-const config = new pulumi.Config()
-const data = config.requireObject<InfrastructureArgs>("data")
+const config = new pulumi.Config();
+const data = config.requireObject<InfrastructureArgs>("data");
 
 export const datahub = new Datahub("datahub", {
-    name: data.datahub.namespace
-})
+  name: data.datahub.namespace,
+});
