eb636acf57eb67aa41ae6755f2c12c631394dc53
svnb <sven@bytecause.ch>
2023-12-02 21:59:34 +0100

    init

---
+++ index.ts
@@ -0,0 +1,15 @@
+import * as pulumi from "@pulumi/pulumi";
+import { Datahub } from "./src/datahub";
+
+interface InfrastructureArgs {
+    datahub: {
+        namespace: string
+    }
+}
+
+const config = new pulumi.Config()
+const data = config.requireObject<InfrastructureArgs>("data")
+
+export const datahub = new Datahub("datahub", {
+    name: data.datahub.namespace
+})
