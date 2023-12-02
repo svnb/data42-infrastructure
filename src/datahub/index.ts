d68d4db0c097c8a8948d282881ab24d8a4a50d4e
svnb <sven@bytecause.ch>
2023-12-02 22:42:32 +0100

    add lint, remove subchart downloads

--- src/datahub/index.ts
+++ src/datahub/index.ts
@@ -1,29 +1,32 @@
-import * as pulumi from "@pulumi/pulumi"
-import { DatahubSnowflakeComponent } from "./snowflake"
-import { DatahubK8sComponent } from "./k8s"
+import * as pulumi from "@pulumi/pulumi";
+import { DatahubSnowflakeComponent } from "./snowflake";
+import { DatahubK8sComponent } from "./k8s";
 
 interface DatahubArgs {
-    name: string
+  name: string;
 }
 
 export class Datahub extends pulumi.ComponentResource {
+  snowflake: DatahubSnowflakeComponent;
+  k8s: DatahubK8sComponent;
 
-    snowflake: DatahubSnowflakeComponent
-    k8s: DatahubK8sComponent
+  constructor(
+    name: string,
+    args: DatahubArgs,
+    opts?: pulumi.ComponentResourceOptions,
+  ) {
+    super("pkg:index:Datahub", name, opts);
 
-    constructor(name: string, args: DatahubArgs, opts?: pulumi.ComponentResourceOptions) {
-        super("pkg:index:Datahub", name, opts)
+    this.snowflake = new DatahubSnowflakeComponent("snowflake", {
+      name: args.name,
+    });
+    this.k8s = new DatahubK8sComponent("k8s", {
+      namespace: args.name,
+    });
 
-        this.snowflake = new DatahubSnowflakeComponent("snowflake", {
-            name: args.name
-        })
-        this.k8s = new DatahubK8sComponent("k8s", {
-            namespace: args.name
-        })
-
-        this.registerOutputs({
-            snowflake: this.snowflake,
-            k8s: this.k8s
-        })
-    }
+    this.registerOutputs({
+      snowflake: this.snowflake,
+      k8s: this.k8s,
+    });
+  }
 }
